830ea1a7f9b4be6a234fb949f0d91daba416023a
Prince Sinha <sinhaprince013@gmail.com>
2020-10-13 00:40:16 +0530

    unstake compare fix

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -303,7 +303,7 @@ class Header extends Component {
               <br/>
               <i>{this.state.retirementYeldCurrentStaked <= 0 ? 
               '' : 
-              `Currently Staked ${window.web3.utils.fromWei(this.state.retirementYeldCurrentStaked)} YELD`}
+              `Currently Staked ${window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked))} YELD`}
               </i>
             </Typography>
           </Button>
@@ -450,9 +450,9 @@ class Header extends Component {
                 onChange={ (e) => this.setState({unStakeAmount: e.target.value}) }
                 placeholder="0"
                 variant="outlined"
-                error={this.state.unStakeAmount > window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked))}
+                error={this.state.unStakeAmount > Number(window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked)))}
                 helperText={
-                  this.state.unStakeAmount > window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked)) ? 
+                  this.state.unStakeAmount > Number(window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked))) ? 
                   `Enter a number less than ${window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked))} (Current Stake)` 
                   : ''
                 }
@@ -465,7 +465,7 @@ class Header extends Component {
                 color="primary"
                 disabled={
                   this.state.unStakeAmount <= 0 || 
-                  this.state.unStakeAmount > window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked))
+                  this.state.unStakeAmount > Number(window.web3.utils.fromWei(String(this.state.retirementYeldCurrentStaked)))
                 }
                 onClick={async () => {
                     await window.retirementYeld.methods.unstake(
